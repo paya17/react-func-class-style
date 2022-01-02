1de2a21c2c0f941baca018595c8bb7c465a903a4
ujiniverse <88028826+ujiniverse@users.noreply.github.com>
2022-01-02 21:45:22 +0900

    class-state

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import React from 'react';
 import './App.css';
 
-function App() { //App컴포넌트
+function App() { 
   return (
-    <div className="container"> {/*이 태그 안에 들어있는 게 'App컴포넌트의 내용'*/}
+    <div className="container"> 
       <h1>Hello World</h1>
-      <FuncComp></FuncComp> {/*FuncComp컴포넌트*/}
-      <ClassComp></ClassComp> {/*ClassComp컴포넌트*/}
+      <FuncComp></FuncComp> 
+      <ClassComp initNumber={2}></ClassComp> {/*ClassComp컴포넌트에, initNumber라는 'props' 주기*/}
     </div>
   );
 }
 
 //--------------------------------------------------------------------------------------//
 
-function FuncComp() { //FuncComp컴포넌트('함수' 스타일 컴포넌트)
+function FuncComp() { 
   return (
     <div className="container">
       <h2>function style component</h2>
@@ -21,11 +21,21 @@ function FuncComp() { //FuncComp컴포넌트('함수' 스타일 컴포넌트)
   );
 }
 
-class ClassComp extends React.Component { //ClassComp컴포넌트('클래스' 스타일 컴포넌트)
+class ClassComp extends React.Component { 
+  state ={
+    number: this.props.initNumber
+  } //'state'의 초깃값으로, 외부에서 전달받은 'props'값을 지정
+
   render() {
     return (
       <div className="container">
         <h2>class style component</h2>
+        <p>Number: {this.state.number}</p> {/*'state값' 사용*/}
+        <input type="button" value="random" onClick={
+          function() {
+            this.setState({number:Math.random()}); //setState 이용해, 'state값' 변경 -> ClassComp컴포넌트의 *render메서드가 호출됨
+          }.bind(this)
+        }></input> {/* *누르면 state의 number의 초깃값이었던 2를 임의의 값으로 변경시켜, state값 변경*/}
       </div>
     );
   }
@@ -40,14 +50,8 @@ export default App;
 - *'함수' 스타일로 '컴포넌트' 만들기 -> 최신 기능인 '훅'을 도입해, 컴포넌트 내부의 <state>와, 컴포넌트의 <라이프사이클>을 다룰 수 있게 됨
 */
 
-/*
--함수 스타일로 컴포넌트 만들 때 -> 'return값'만 만들면, 그게 바로 컴포넌트의 실제 모양
--클래스 스타일로 컴포넌트 만들 때 -> 'render메서드' 정의한 후, 그것의 'return값'이 컴포넌트의 실제 모양
-*/
-
-//FuncComp컴포넌트와 ClassComp컴포넌트를 저번처럼 따로 컴포넌트 파일을 만들어서 빼내지 않고 App.js에 적었다
-
-
-
+//여기부터
+//'클래스' 스타일 컴포넌트의 props와 state
+//state를 설정하고 초기화하고, 그 state값을 사용하고, 변경하기
 
 

e3ee2f9bf92973110f52381d5ffdf696d7e35b1a
ujiniverse <88028826+ujiniverse@users.noreply.github.com>
2022-01-03 23:47:40 +0900

    function-LifeCycle-useEffect

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'; 
+import React, {useState, useEffect} from 'react'; //useEffect함수를 사용하기 위해 import
 import './App.css';
 
 function App() { 
@@ -16,13 +16,18 @@ function App() {
 function FuncComp(props) { 
    
   var [number, setNumber] = useState(props.initNumber);  
-  var [_date, setDate] = useState((new Date()).toString()); //('state'의 초깃값 지정) 
+  var [_date, setDate] = useState((new Date()).toString()); 
+
+  //side effect
+  useEffect(function() {
+    document.title = number + ':' + _date; //화면이 아닌 title값을 바꾸는 작업(*컴포넌트가 렌더링되는 작업과 상관X)->useEffect함수의 'side effect'기능
+  }); //*useEffect함수 사용! (컴포넌트 return전에 작성?!, useEffect함수의 첫번째 인자로 함수가 들어와야 함)
 
   return (
     <div className="container">
       <h2>function style component</h2>
       <p>Number : {number}</p> 
-      <p>Date : {_date}</p> {/*'state값' 사용*/}
+      <p>Date : {_date}</p> 
       <input type="button" value="random" onClick={
           function() {
             setNumber(Math.random()); 
@@ -30,7 +35,7 @@ function FuncComp(props) {
         }></input> {/*임의의 값을 발생시키는 random버튼*/}
       <input type="button" value="date" onClick={
           function() {
-            setDate((new Date()).toString()); //'state값' 변경
+            setDate((new Date()).toString()); 
           }
         }></input> {/*현재시각이 나오는 date버튼*/}
     </div>
@@ -40,7 +45,7 @@ function FuncComp(props) {
 class ClassComp extends React.Component { 
   state ={
     number: this.props.initNumber,
-    date: (new Date()).toString() //'state'의 초깃값 지정
+    date: (new Date()).toString() 
   } 
 
   render() {
@@ -48,17 +53,17 @@ class ClassComp extends React.Component {
       <div className="container">
         <h2>class style component</h2>
         <p>Number: {this.state.number}</p> 
-        <p>Date: {this.state.date}</p> {/*'state값' 사용*/}
+        <p>Date: {this.state.date}</p> 
         <input type="button" value="random" onClick={
           function() {
             this.setState({number:Math.random()}); 
           }.bind(this)
-        }></input> {/*임의의 값을 발생시키는 random버튼*/}
+        }></input> 
         <input type="button" value="date" onClick={
           function() {
-            this.setState({date:(new Date()).toString()}); //'state값' 변경
+            this.setState({date:(new Date()).toString()}); 
           }.bind(this)
-        }></input> {/*현재시각이 나오는 date버튼*/}
+        }></input> 
       </div>
     );
   }
@@ -73,8 +78,9 @@ export default App;
 - *'함수' 스타일로 '컴포넌트' 만들기 -> 최신 기능인 '훅'을 도입해, 컴포넌트 내부의 <state>와, 컴포넌트의 <라이프사이클>을 다룰 수 있게 됨
 */
 
-//여기부터
 //컴포넌트의 '생로병사'와 관련된 기능이, '라이프사이클'
+//클래스 스타일 컴포넌트에서 라이프사이클API를 구현하는 방법 / 함수 스타일 컴포넌트에서 라이프사이클API를 구현하는 방법
+
 /*
 '클래스' 스타일 컴포넌트의 라이프사이클 
 : 'componentWillMount메서드'가 호출됨(컴포넌트가 mount되기 전에(render메서드가 호출되기 전에), 처리해야 할 일을 함) -> 'render메서드'가 호출돼서, mount됨(화면에 그려짐)
@@ -87,8 +93,16 @@ V->컴포넌트가 한번 만들어진 후에, state/props가 바뀌는 '변화'
 //*클래스 스타일 컴포넌트의 라이프사이클에서 원하는 타이밍에 코드를 추가하고 싶으면, 정해진 이름의 메서드(라이프사이클API)를 구현
 //mount:화면에 그려짐
 
-
-
+//여기부터
+//함수 스타일 컴포넌트가 실행된 후에, 추가로 필요한 작업을 처리하기 위해 -> *"useEffect함수(훅)"를 이용 
+/*
+'함수' 스타일 컴포넌트의 라이프사이클 (완성X)
+: render -> 'useEffect함수'의 인자로 전달한 함수가 호출됨 -> state/props가 바뀌는 '변화'가 생김 -> render -> 'useEffect함수'의 인자로 전달한 함수가 호출됨
+*/
+//*useEffect함수 -> render가 실행될때마다 그 후에 실행됨 (클래스 스타일 컴포넌트의 componentDidMount,componentDidUpdate와 같은 효과)
+//컴포넌트의 main effect는 FuncComp이라는 함수가 호출됐을 때 return값을 화면에 그려주는 작업 / side effect는 컴포넌트가 화면에 render된 후에, 컴포넌트의 정보를 다른데서 가져와 나중에 내용을 변경하거나 네트워크 통신 등의 작업
+//*useEffect함수 -> 'side effect'기능을 함 (클래스 스타일 컴포넌트의 componentDidMount,componentDidUpdate와 같은 효과)
+//useEffect를 여러개 설치할 수 있다
 
 
 
